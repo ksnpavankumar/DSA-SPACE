e4d261b88a416480051d3abe81a1be949142e00e
pavankunasani <pavankunasani@users.noreply.github.com>
2024-12-23 23:54:24 +0530

    Update StackProcessor.java

--- dsa3/day-40-stack/StackProcessor.java
+++ dsa3/day-40-stack/StackProcessor.java
@@ -49,6 +49,7 @@ public class StackProcessor {
         System.out.println("Result for test case 4: " + processor.processStack(A4, B4, C4));  // Expected output: 4
     }
 
+
     /*
      * Intuition:
      * 1. The solution simulates stack behavior based on the input array C.
@@ -60,5 +61,31 @@ public class StackProcessor {
      *    - If the stack is empty, return -1.
      *    - Otherwise, return the top element of the stack.
      * The solution has a time complexity of O(A), where A is the size of the array C.
+     * 
+     * ==================
+     * There is a football event going on in your city. In this event, you are given A passes and players having ids between 1 and 106.
+     * 
+     * Initially, some player with a given id had the ball in his possession. You have to make a program to display the id of the player who possessed the ball after exactly A passes.
+     * 
+     * There are two kinds of passes:
+     * 
+     * 1) ID
+     * 2) 0
+     * 
+     * For the first kind of pass, the player in possession of the ball passes the ball "forward" to the player with id = ID.
+     * 
+     * For the second kind of pass, the player in possession of the ball passes the ball back to the player who had forwarded the ball to him.
+     * 
+     * In the second kind of pass "0" just means Back Pass.
+     * 
+     * Return the ID of the player who currently possesses the ball.
+     * 
+     * Problem Constraints:
+     * 
+     * 1 <= A <= 100000
+     * 
+     * 1 <= B <= 100000
+     * 
+     * |C| = A
      */
 }
